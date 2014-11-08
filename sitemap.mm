66256c1626c5adfa33b26fd40d27f90c9dff9bc7
Andrei Matveyeu <andrei@ideabulbs.com>
2014-11-08 10:11:27 +0100

    future sitemap added

---
+++ sitemap.mm
@@ -0,0 +1,10 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1415437698621" ID="ID_1865902038" MODIFIED="1415437854275" TEXT="Andrei Matveyeu">
+<node CREATED="1415437754862" ID="ID_109836644" MODIFIED="1415437775721" POSITION="left" TEXT="code :: github"/>
+<node CREATED="1415437726552" ID="ID_637713530" MODIFIED="1415437807785" POSITION="left" TEXT="Andrei Matveyeu :: facebook"/>
+<node CREATED="1415437716362" ID="ID_408653330" MODIFIED="1415437794778" POSITION="right" TEXT="CV :: LinkedIn"/>
+<node CREATED="1415437746375" ID="ID_1510374012" MODIFIED="1415437824216" POSITION="right" TEXT="play the bass! :: youtube"/>
+<node CREATED="1415437840457" ID="ID_195410111" MODIFIED="1415437848254" POSITION="right" TEXT="work"/>
+</node>
+</map>
